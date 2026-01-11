ced14d0ff89a60308199031ce2c12d15057bdec1
Snigdha Srivatsava <snigdhasrivatsava1@gmail.com>
2026-01-11 15:24:52 -0500

    Updated Experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,7 +11,7 @@ const experiences = [
   {
     id: 1,
     company: "Manning College of Information and Computer Sciences",
-    role: "Software Developer",
+    role: "Software Engineer",
     period: "Feb 2025 – Present",
     location: "Amherst, MA",
     image:
